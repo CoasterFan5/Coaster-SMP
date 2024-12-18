86c46a15640c6435b4c6ada687929a4ed9f7a5d7
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-12-18 13:48:56 -0500

    update prisma export for esm

--- db/db.ts
+++ db/db.ts
@@ -1,3 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import type { PrismaClient as PrismaClientType } from "@prisma/client";
+import { createRequire as cr } from "node:module";
+
+const r = cr(import.meta.url ?? __filename);
+
+const { PrismaClient: PrismaClientImpl } = r("@prisma/client");
+
+export class PrismaClient extends (PrismaClientImpl as typeof PrismaClientType) {}
 
 export const prisma = new PrismaClient();
